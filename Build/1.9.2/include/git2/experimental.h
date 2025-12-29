a9a10e4dfdd2c18fb5c281788c8df66b934b7380
Vault Thirteen <54213347+vault-thirteen@users.noreply.github.com>
2025-12-30 02:06:59 +0300

    ...

--- Build/1.9.2/include/git2/experimental.h
+++ Build/1.9.2/include/git2/experimental.h
@@ -8,13 +8,6 @@
 #ifndef INCLUDE_experimental_h__
 #define INCLUDE_experimental_h__
 
-/*
- * This file exists to support users who build libgit2 with a bespoke
- * build system and do not use our cmake configuration. Normally, cmake
- * will create `experimental.h` from the `experimental.h.in` file and
- * will include the generated file instead of this one. For non-cmake
- * users, we bundle this `experimental.h` file which will be used
- * instead.
- */
+/* #undef GIT_EXPERIMENTAL_SHA256 */
 
 #endif
